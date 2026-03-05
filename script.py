2e1d62c5350e856daf54bf0ab30f5ba5278e13df
manuelneveu-ui <manuel.neveu@2029.icam.fr>
2026-03-05 14:58:08 +0100

    Initial commit

---
+++ script.py
@@ -0,0 +1,14 @@
+import os
+
+folder = r"pictures and video/Converted .PNG"
+extensions = (".jpg", ".jpeg", ".png")  # converted images
+
+tags = []
+for name in sorted(os.listdir(folder)):
+    if name.lower().endswith(extensions):
+        path = f"{folder}/{name}"
+        tags.append(f'  <img src="{path}" class="media-thumb" alt="">')
+
+print("<div class=\"media-gallery\">")
+print("\n".join(tags))
+print("</div>")
